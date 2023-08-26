9cfa776ec41cd1b4394b5a3698418a0781f16318
Kursad <kursad.demirdogen@gmail.com>
2023-08-26 13:52:51 +0300

    Add project files.

---
+++ SignalRSeverExample/Hubs/MyHub.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalRSeverExample.Hubs
+{
+    public class MyHub : Hub 
+    {
+        public async Task SendMessageAsync (string message)
+         {
+            await Clients.All.SendAsync("receiveMessage", message);
+            //client tarafından gönderilen "message" ı ala tüm clientlardaki "receiveMessage" 
+            //isimli metodu tetikle ve "message" ı tüm clientlara yolla
+        }
+    }
+}
